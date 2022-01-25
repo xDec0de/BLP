0d5e00c708134b16a9b4325b1c4c6cc5ffe02148
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-01-25 01:26:33 +0100

    Added BLPCfg back (Whoops!)

---
+++ src/es/xdec0de/blp/utils/files/BLPCfg.java
@@ -0,0 +1,41 @@
+package es.xdec0de.blp.utils.files;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import es.xdec0de.blp.BLP;
+import net.md_5.bungee.api.ChatColor;
+
+public class BLPCfg {
+
+	private static FileConfiguration cfg;
+	private static File file;
+	
+	public static void setup() {
+		if (!BLP.getInstance().getDataFolder().exists())
+			BLP.getInstance().getDataFolder().mkdir(); 
+		if (!(file = new File(BLP.getInstance().getDataFolder(), "config.yml")).exists())
+			BLP.getInstance().saveResource("config.yml", false); 
+		cfg = (FileConfiguration)YamlConfiguration.loadConfiguration(file);
+		}
+
+	public static void save() {
+		try {
+			cfg.save(file);
+		} catch (IOException e) {
+			Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&e&lBattleLevelsPAPI: &cCould not save &6config.yml&c file."));
+		} 
+	}
+
+	public static FileConfiguration get() {
+		return cfg;
+	}
+
+	public static void reload() {
+		cfg = (FileConfiguration)YamlConfiguration.loadConfiguration(file);
+	}
+}
