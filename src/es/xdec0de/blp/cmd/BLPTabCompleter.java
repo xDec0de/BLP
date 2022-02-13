2e4280c2ddf669ec7fc4f105abc90ba58c07e805
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-02-13 18:15:49 +0100

    Added permission support to tab completer

--- src/es/xdec0de/blp/cmd/BLPTabCompleter.java
+++ src/es/xdec0de/blp/cmd/BLPTabCompleter.java
@@ -1,18 +1,26 @@
 package es.xdec0de.blp.cmd;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
+import es.xdec0de.blp.utils.files.BLPCfg;
+import es.xdec0de.blp.utils.files.enums.BLPSetting;
+
 public class BLPTabCompleter implements TabCompleter {
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-		if(args.length == 1)
-			return Arrays.asList(new String[]{"reload", "checkupdates"});
-		return null;
+		ArrayList<String> tab = new ArrayList<String>();
+		if(args.length == 1) {
+			if(BLPCfg.hasPermission(BLPSetting.RELOAD_PERMISSION, sender, false))
+				tab.add("reload");
+			if(BLPCfg.hasPermission(BLPSetting.UPDATE_CHECK_PERMISSION, sender, false))
+				tab.add("checkupdates");
+		}
+		return tab;
 	}
 }
