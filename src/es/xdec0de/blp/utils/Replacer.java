4cf6e7e637246ed92dae04d7b22123a6c77c3da0
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-02-13 18:56:04 +0100

    Removed more unused methods (Replacers)

--- src/es/xdec0de/blp/utils/Replacer.java
+++ src/es/xdec0de/blp/utils/Replacer.java
@@ -22,13 +22,13 @@ public class Replacer {
 	 * create a replacer variable and apply it to as many strings as you want to <b>avoid creating multiple instances of the same replacements</b>, also, 
 	 * make sure that the amount of strings added to the replacer are <b>even</b>, otherwise, an exception will be thrown!
 	 * 
-	 * @param replaces The strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
+	 * @param replacements The strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
 	 * 
 	 * @see #replaceAt(String)
 	 * @see #replaceAt(List)
 	 */
-	public Replacer(String... replaces) {
-		replaceList.addAll(Arrays.asList(replaces));
+	public Replacer(String... replacements) {
+		replaceList.addAll(Arrays.asList(replacements));
 		if(replaceList.size() % 2 != 0)
 			throw new IllegalArgumentException(replaceList.get(replaceList.size() - 1) + "does not have a replacer! Add one more element to the replacer.");
 	}
@@ -37,28 +37,16 @@ public class Replacer {
 	 * Adds new strings to an existing replacer, the amount of strings must also be even, note that existing replacements won't be ignored 
 	 * and the new replacer won't overwrite them.
 	 * 
-	 * @param replaces The new strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
+	 * @param replacements The new strings to be replaced, the format is "%placeholder1%", "replace1", "%placeholder2%", "replace2"...
 	 * 
 	 * @return The old replacer with the new replacements added to it.
 	 * 
 	 * @throws IllegalArgumentException if the amount of strings is not even, more technically, if replaces size % 2 is not equal to 0.
 	 */
-	public Replacer add(String... replaces) {
-		if(replaces.length % 2 != 0)
-			throw new IllegalArgumentException(replaces[replaces.length -1] + "does not have a replacer! Add one more element to the replacer.");
-		replaceList.addAll(Arrays.asList(replaces));
-		return this;
-	}
-
-	/**
-	 * Adds a replacer to an existing replacer, joining them, note that existing replacements won't be ignored and the new replacer won't overwrite them.
-	 * 
-	 * @param replacer The replacer to add.
-	 * 
-	 * @return The old replacer with the new replacements added to it.
-	 */
-	public Replacer add(Replacer replacer) {
-		replaceList.addAll(replacer.replaceList);
+	public Replacer add(String... replacements) {
+		if(replacements.length % 2 != 0)
+			throw new IllegalArgumentException(replacements[replacements.length -1] + "does not have a replacer! Add one more element to the replacer.");
+		replaceList.addAll(Arrays.asList(replacements));
 		return this;
 	}
 
@@ -75,21 +63,6 @@ public class Replacer {
 		String res = str;
 		for(int i = 0; i <= replaceList.size() - 1; i += 2)
 			res = res.replace(replaceList.get(i), replaceList.get(i + 1));
-		return res;
-	}
-
-	/**
-	 * Applies the replacements to the specified list of strings, it the list is null, null will be returned.
-	 * 
-	 * @param str The string list to apply the replacements to.
-	 * 
-	 * @return A new string list with the replacements applied to it.
-	 */
-	public List<String> replaceAt(List<String> list) {
-		if(list == null)
-			return null;
-		List<String> res = new LinkedList<String>();
-		list.forEach(str -> res.add(replaceAt(str)));
 		return res;
 	}
 }
--- src/es/xdec0de/blp/utils/files/BLPMessage.java
+++ src/es/xdec0de/blp/utils/files/BLPMessage.java
@@ -47,17 +47,6 @@ public enum BLPMessage {
 	}
 
 	/**
-	 * Gets a message with colors {@link #applyColor(String)} and the default {@link Replacer}, also, the specified replacer is added to the default replacer.
-	 * 
-	 * @param replacer The replacer to apply.
-	 * 
-	 * @return The message as a string, with colors and replacer applied to it.
-	 */
-	public String asString(Replacer replacer) {
-		return MessageUtils.applyColor(new Replacer("%prefix%", MessageUtils.prefix, "%error%", MessageUtils.errorPrefix).add(replacer).replaceAt(BLPMessages.get().getString(path)));
-	}
-
-	/**
 	 * Gets a message with colors {@link #applyColor(String)} and the default {@link Replacer}, also, a new replacer made with the 
 	 * specified strings is added to the default replacer.
 	 * 
@@ -80,21 +69,6 @@ public enum BLPMessage {
 	 */
 	public void send(CommandSender sender) {
 		String send = asString();
-		if(!send.isEmpty())
-			sender.sendMessage(send);
-	}
-
-	/**
-	 * Sends a message with colors {@link #applyColor(String)} and the default {@link Replacer}, also, 
-	 * the specified replacer is added to the default replacer, empty messages will be ignored and the message wont be sent.
-	 * 
-	 * @param sender The sender that will receive the message.
-	 * @param replacer The replacer to apply.
-	 * 
-	 * @see #getMessage(BLPMessage, Replacer)
-	 */
-	public void send(CommandSender sender, Replacer replacer) {
-		String send = asString(replacer);
 		if(!send.isEmpty())
 			sender.sendMessage(send);
 	}
